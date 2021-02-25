b9b6fefb70edce3f3bc6ddd9664e23e640cc92b2
Timon Hüppi <timon.hueppi@gmail.com>
2021-02-25 13:49:49 +0100

    sets up the UI

--- app/src/main/java/ch/timonhueppi/m335/senseit/MainActivity.java
+++ app/src/main/java/ch/timonhueppi/m335/senseit/MainActivity.java
@@ -2,9 +2,15 @@ package ch.timonhueppi.m335.senseit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
