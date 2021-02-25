fd145089227bc8e814cdde90b2192948a5817593
Timon Hüppi <timon.hueppi@gmail.com>
2021-02-25 13:20:17 +0100

    Adds initial project files

---
+++ app/src/main/java/ch/timonhueppi/m335/senseit/MainActivity.java
@@ -0,0 +1,14 @@
+package ch.timonhueppi.m335.senseit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
